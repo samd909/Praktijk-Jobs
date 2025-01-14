b81185d5e6c5478aaab3717050bae8de90e41bc9
samd909 <112548067+samd909@users.noreply.github.com>
2025-01-14 11:32:02 +0100

    GOOGLE LOGIN GEADD
    GOOGLE LOGIN GEADD

--- praktijkjobs/client/views/home.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-
-def home(request):
-    template = "home.html"
-    context = {
-
-    }
-    return render(request, template, context)
--- praktijkjobs/static/js/previewImage.js
+++
@@ -1,8 +0,0 @@
-function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var output = document.getElementById('profileImg');
-        output.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
-}
